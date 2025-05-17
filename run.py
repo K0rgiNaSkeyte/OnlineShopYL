c962d856580f3c3740414722d22ed2edf0b78e72
Korgi_na_skeyte <vereshagindanila2007@gmail.com>
2025-05-17 15:50:50 +0300

    Fix "run.py"

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
 from app import create_app
+from config import config
 
-app = create_app(os.getenv('FLASK_ENV', 'production'))
+# Получаем конфигурацию на основе переменной окружения
+env = os.getenv('FLASK_ENV', 'production')
+app = create_app(config[env])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
