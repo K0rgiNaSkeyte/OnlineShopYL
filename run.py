7cc7a27937338987f9ce90c29f913d26bedd1111
Korgi_na_skeyte <vereshagindanila2007@gmail.com>
2025-05-17 15:55:19 +0300

    Fix "No module named production" error

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
 from app import create_app
-from config import config
+from config import ProductionConfig, DevelopmentConfig
 
-# Получаем конфигурацию на основе переменной окружения
-env = os.getenv('FLASK_ENV', 'production')
-app = create_app(config[env])
+# Выбираем конфигурацию на основе переменной окружения
+if os.getenv('FLASK_ENV') == 'development':
+    app = create_app(DevelopmentConfig)
+else:
+    app = create_app(ProductionConfig)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
